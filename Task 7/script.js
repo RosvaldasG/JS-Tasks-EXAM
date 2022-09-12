b3d5fca58000a339d728d7c34dc0596b0178abbc
RosvaldasG <rosvaldasggg@gmail.com>
2022-09-12 18:35:36 +0300

    Egzaminas JS

---
+++ Task 7/script.js
@@ -0,0 +1,20 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+
+function showObjectKeys(data) {
+  return Object.keys(data);
+}
+
+const key = showObjectKeys(audi);
+
+console.log(key);
